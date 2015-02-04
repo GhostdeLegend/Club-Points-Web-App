0a7b8e30686d1b86bd6997a7b4256c81874b1ada
GhostdeLegend <rustin.harris@hotmail.com>
2015-02-03 21:01:35 -0600

    First Add

---
+++ _vti_cnf/process.php
@@ -0,0 +1,15 @@
+vti_encoding:SR|utf8-nl
+vti_author:SR|R-PC\\R
+vti_modifiedby:SR|R-PC\\R
+vti_timelastmodified:TR|30 Jan 2015 18:48:22 -0000
+vti_timecreated:TR|30 Jan 2015 18:26:46 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|createEvent.html index.html
+vti_nexttolasttimemodified:TW|30 Jan 2015 18:29:32 -0000
+vti_cacheddtm:TX|30 Jan 2015 18:48:22 -0000
+vti_filesize:IR|485
+vti_cachedneedsrewrite:BR|false
+vti_cachedhasbots:BR|false
+vti_cachedhastheme:BR|false
+vti_cachedhasborder:BR|false
+vti_charset:SR|utf-8
---
+++ process.php
@@ -0,0 +1,23 @@
+<?php
+
+$specialSelect = $_POST['specialSelect'];
+$event = $_POST['event'];
+$day = $_POST['day'];
+$month = $_POST['month'];
+$year = $_POST['year'];
+$hour = $_POST['hour'];
+$minute = $_POST['minute'];
+$ampm = $_POST['ampm'];
+$comment = $_POST['comment'];
+
+echo $_POST['specialSelect'];
+echo $_POST['event'];
+echo $_POST['day'];
+echo $_POST['month'];
+echo $_POST['year'];
+echo $_POST['hour'];
+echo $_POST['minute'];
+echo $_POST['ampm'];
+echo $_POST['comment'];
+
+?>
